2fb0ea0b7163ceceaceee07b891c8a4eaae39dd9
Humberto Sandigo <sandigohumberto@gmail.com>
2016-11-04 10:54:25 -0500

    progress commit #8? - updates in js, html, css -- js still not functional - no text is incorrect, it's just not working

--- simplesimon.js
+++ simplesimon.js
@@ -1,119 +1,133 @@
- "use strict";
- var settings = {
-     sequence: [],
-     round: 0,
-     playNumber: 0,
-     speed: 1000,
-     clicked: 0
- };
+"use strict";
+var settings = {
+    sequence: [],
+    round: 0,
+    playNumber: 0,
+    speed: 1000,
+    clicked: 0
+};
 
- $(document).ready(function() {
-     function animate(liid) {
+//START and END GAME
 
-         // Increase round speed.
-         if (settings.round > 5) {
-             settings.speed = 500
-         }
-         if (liid == "box1") {
-             $("#box1").css("border-color", "#1aff00");
-             setTimeout(function() {
-                 $("#box1").css("border-color", "#0b7000");
-             }, 200);
-         } else if (liid == "box2") {
-             $("#box2").css("border-color", "#ff0b00");
-             setTimeout(function() {
-                 $("#box2").css("border-color", "#c30800");
-             }, 200);
-         } else if (liid == "box3") {
-             $("#box3").css("border-color", "#ffec00");
-             setTimeout(function() {
-                 $("#box3").css("border-color", "#c3b400");
-             }, 200);
-         } else if (liid == "box4") {
-             $("#box4").css("border-color", "#29abd0");
-             setTimeout(function() {
-                 $("#box4").css("border-color", "#196d85");
-             }, 200);
-         }
-     }
+$(document).ready(function () {
+$("#box1, #box2, #box3, #box4").on("click", function () {
+    animate(this.id)
+});
+$("#startGame").on("click", function () {
+    $("#startGame").hide();
+    // $("#simon, #count").css("filter", "blur(0px)");
+    // $("#simon, #count").css("-webkit-filter", "blur(0px)");
+    settings.round++;
+    makeId(); // make id and play it
+});
 
-     function makeid() {
-         var text = "";
-         var possible = "1234"; //or should this be "box1box2box3box4"
+$("#resetGame").on("click", function () {
+    $("#resetGame").hide();
+    settings.sequence = [];
+    settings.round = 0;
+    settings.playNumber = 0;
+        settings.speed = 1000;
+    settings.clicked = 0;
+    $("#startGame").trigger("click");
+});
 
-         for (var i = 0; i < 1; i++) {
+    function animate(liid) {
+
+        // Increase round speed.
+        if (settings.round > 5) {
+            settings.speed = 500
+        }
+        if (liid == "box1") {
+            $("#box1").css("border-color", "#C6E2A7");//new
+            setTimeout(function () {
+                $("#box1").css("border-color", "#E7F4E1");//original
+            }, 200);
+        } else if (liid == "box2") {
+            $("#box2").css("border-color", "#67EF8B");//new
+            setTimeout(function () {
+                $("#box2").css("border-color", "#91FABB");//orig
+            }, 200);
+        } else if (liid == "box3") {
+            $("#box3").css("border-color", "#31A545");//new
+            setTimeout(function () {
+                $("#box3").css("border-color", "#41E181");//orig
+            }, 200);
+        } else if (liid == "box4") {
+            $("#box4").css("border-color", "#A6DBAB");
+            setTimeout(function () {
+                $("#box4").css("border-color", "#C6EFD7");//orig
+            }, 200);
+        }
+    }
+
+    function makeId() {
+        var text = "";
+        var possible = "box1box2box3box4";
+
+        // var random = Math.floor((Math.random() * 4) + 1);
+        // for (var i = 1; i >= 4; i++) {
+        //     console.log('Loop counter is: ' + i);
+        // }
+         for (var i = 1; i >= 4; i++) {
              text += possible.charAt(Math.floor(Math.random() * possible.length));
              settings.sequence.push(text);
-         }
-         function myLoop() {
-             setTimeout(function() {
-                 animate(settings.sequence[settings.playNumber]);
-                 settings.playNumber++;
-                 if (settings.playNumber < settings.sequence.length) {
-                     myLoop();
-                 } else {
-                     settings.playNumber = 0;
-                     listen();
-                 }
-             }, settings.speed)
-         }
-         myLoop();
-     }
+        }
+        function myLoop() {
+            setTimeout(function () {
+                animate(settings.sequence[settings.playNumber]);
+                settings.playNumber++;
+                if (settings.playNumber < settings.sequence.length) {
+                    myLoop();
+                } else {
+                    settings.playNumber = 0;
+                    listen();
+                }
+            }, settings.speed)
+        }
 
-     // LISTEN
-     function listen() {
+        myLoop();
+    }
 
-         $("#box1, #box2, #box3, #box4").on("mousedown", function() {
-             if (this.id == settings.sequence[settings.clicked]) {
+    // LISTEN
+    function listen() {
 
-                 if (settings.clicked === settings.sequence.length - 1) {
-                     $("#box1, #box2, #box3, #box4").off("mousedown");
-                     settings.clicked = 0;
-                     $("#start").trigger("click");
-                 } else {
-                     console.log("Right!");
-                     settings.clicked++;
-                 }
-             } else {
-                 console.log("WRONG");
-                 $("#fail").show();
-                 $("#fail").addClass("bigEntrance");
-                 $("#simon, #count").css("filter", "blur(5px)");
-                 $("#simon, #count").css("-webkit-filter", "blur(5px)");
-                 settings.clicked = 0;
-                 $("#box1, #box2, #box3, #box4").off("mousedown");
-             }
-         });
-     }
-     //BEGIN GAME
+        $("#box1, #box2, #box3, #box4").on("mouseDown", function () {
+            if (this.id === settings.sequence[settings.clicked]) {
 
-     $("#box1, #box2, #box3, #box4").on("click", function() {
-         animate(this.id)
-     });
-     $("#startgame").on("click", function() {
-         $("#startgame").hide();
-         // $("#simon, #count").css("filter", "blur(0px)");
-         // $("#simon, #count").css("-webkit-filter", "blur(0px)");
-         settings.round++;
-         makeid(); // make id and play it
-     });
+                if (settings.clicked === settings.sequence.length - 1) {
+                    $("#box1, #box2, #box3, #box4").off("mouseDown");
+                    settings.clicked = 0;
+                    $("#startGame").trigger("click");
+                } else {
+                    console.log("Right!");
+                    settings.clicked++;
+                }
+            } else {
+                console.log("WRONG");
+                $("#resetGame").show();
+                $("#resetGame").addClass("bigEntrance");
+                // settings.clicked = 0;
+                $("#box1, #box2, #box3, #box4").off("mouseDown");
+            }
+        });
+    }
 
-     $("#fail").on("click", function() {
-         $("#fail").hide();
-         settings.sequence = [];
-         settings.round = 0;
-         settings.playNumber = 0,
-             settings.speed = 1000;
-         settings.clicked = 0;
-         $("#start").trigger("click");
-     });
- });
+    // count- effects ---slides down from footer?
+    $('#count-toggle').click(function() {
+        $('#count').slideDown();
+    });
+
+});
+
+//math.random sequence
 
 
+//possible ternary operator for messages once the game ends
+//po
 //var message = (success) ? "Next Round!" : "Game Over!";
 
-
- //     // $("#boxes").click(function (addEventListener, click) {
+//possible animation for boxes once the game works
+//     // $("#boxes").click(function (addEventListener, click) {
 //     //     console.log("you clicked on a box")
 //     // });
 //     //
@@ -134,29 +148,22 @@
 //      }
 //  }
 //
-//
-//  var random = Math.floor((Math.random()*4)+1);
-//  for (var i = 1; i >= 4; i++) {
-//      console.log('Loop counter is: ' + i);
+// function animateBox(number) {
+//  //console.log(number);
+//  switch (number) {
+//      case 1:
+//          flashBox(box1);
+//          break;
+//      case 2:
+//          flashBox(box2);
+//          break;
+//      case 3:
+//          flashBox(box3);
+//          break;
+//      case 4:
+//          flashBox(box4);
 //  }
-//
-// // function animateBox(number) {
-// //  //console.log(number);
-// //  switch (number) {
-// //      case 1:
-// //          flashBox(box1);
-// //          break;
-// //      case 2:
-// //          flashBox(box2);
-// //          break;
-// //      case 3:
-// //          flashBox(box3);
-// //          break;
-// //      case 4:
-// //          flashBox(box4);
-// //  }
-// // }
-//
+// }
 //
 // //Animation for Boxes
 // function flashBox(id) {
@@ -170,7 +177,7 @@
 //
 // startGame.addEventListener("click", startGame);
 
-
+//old way of trying to make this work with vars and event listeners
 //    var boxOne = document.getElementById("boxOne");
 //    var boxTwo = document.getElementById("boxTwo");
 //    var boxThree = document.getElementById("boxThree");
@@ -181,6 +188,7 @@
 //    boxThree.addEventListener("click", boxThreeClicked);
 //    boxFour.addEventListener("click", boxFourClicked);
 
+//didn't know what some of the code in here so I didn't use it
 //    var sequence = ["#boxOne", "#boxTwo", "#boxThree", "#boxFour","#boxOne"];
 //    var human = [];
 //    var i = 0;
@@ -199,6 +207,7 @@
 //
 //        var boxes = [boxOne, boxTwo, boxThree, boxFour];
 
+//possible way of starting the game
 //    function SimonSays( timeLapse,
 //                        elements,
 //                        cssClass,
@@ -231,55 +240,3 @@
 //            }
 //        })()
 //    }
-
-
-
-
-//    var random = Math.floor((Math.random()*4)+1);
-//    for (var i = 1; i >= 4; i++) {
-//
-//        console.log('Loop counter is: ' + i);
-
-//    var boxOne = document.getElementById("boxOne");
-//    var boxTwo = document.getElementById("boxTwo");
-//    var boxThree = document.getElementById("boxThree");
-//    var boxFour = document.getElementById("boxFour");
-//
-//    function boxOneClicked () {
-////        event.preventDefault();
-////        if (middleBox.value == ""){
-////            document.getElementById("left").value += "5";
-////        }else{
-////            document.getElementById("right").value += "5";
-////        }
-//    }
-//    function boxTwoClicked () {
-////        event.preventDefault();
-////        if (middleBox.value == ""){
-////            document.getElementById("left").value += "5";
-////        }else{
-////            document.getElementById("right").value += "5";
-////        }
-//    }
-//    function boxThreeClicked () {
-////        event.preventDefault();
-////        if (middleBox.value == ""){
-////            document.getElementById("left").value += "5";
-////        }else{
-////            document.getElementById("right").value += "5";
-////        }
-//    }
-//    function boxFourClicked () {
-////        event.preventDefault();
-////        if (middleBox.value == ""){
-////            document.getElementById("left").value += "5";
-////        }else{
-////            document.getElementById("right").value += "5";
-////        }
-//    }
-//
-
-
-//    var boxOne = function () {
-//        alert('You clicked the button!');
-//    };
